a177460ebac7eb3c3fbbd231559ae6db5033ac37
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-11-20 11:48:02 +0000

    Step 7

--- numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -51,12 +52,11 @@ public class ListAggregatorTest {
     @Test
     public void distinct() {
         ListAggregator aggregator = new ListAggregator();
-        class StubListSorter implements GenericListSorter{
-            @Override public List<Integer> sort(List<Integer> list) {
-                return  Arrays.asList(1, 2, 2, 4, 5);
-            }
-        }
-        int distinct = aggregator.distinct(list, new ListDeduplicator(new StubListSorter()));
+
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1,2,2,4,5));
+
+        int distinct = aggregator.distinct(list, new ListDeduplicator(sorter));
 
         Assertions.assertEquals(4, distinct);
     }
@@ -65,14 +65,10 @@ public class ListAggregatorTest {
     public void distinct_bug_8726() {
         List<Integer> aList = Arrays.asList(1,2,4,2);
 
-        class StubListDeduplicator implements GenericListDeduplicator{
-            @Override public List<Integer> deduplicate(List<Integer> list) {
-                return  Arrays.asList(1, 2, 4);
-            }
-        }
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
 
         ListAggregator aggregator = new ListAggregator();
-        StubListDeduplicator deduplicator = new StubListDeduplicator();
         int distinct = aggregator.distinct(aList, deduplicator);
 
         Assertions.assertEquals(3, distinct);
