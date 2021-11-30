11ed177f894984b56a2691e3629b2d4d5bb68039
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2021-11-30 20:14:42 +0000

    last test

--- numbers/src/test/groovy/ListSorterTest.groovy
+++ numbers/src/test/groovy/ListSorterTest.groovy
@@ -1,2 +1,31 @@
-class ListSorterTest {
+import com.aor.numbers.ListSorter
+import spock.lang.Specification
+
+class ListSorterTest extends Specification{
+
+    List<Integer> list
+    List<Integer> expected
+
+    def setup() {
+        list = Arrays.asList(3, 2, 6, 1, 4, 5, 7)
+        expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7)
+    }
+
+    def "test sort"() {
+        given:
+        ListSorter sorter = new ListSorter()
+        when:
+        def result = sorter.sort(list)
+        then:
+        result == expected
+    }
+
+    def "test sort bug 8276"() {
+        given:
+        def sorter = new ListSorter()
+        when:
+        def result = sorter.sort(Arrays.asList(1,2,4,2))
+        then:
+        result == Arrays.asList(1,2,2,4)
+    }
 }
--- numbers/src/test/groovy/PositiveFilterTest.groovy
+++ numbers/src/test/groovy/PositiveFilterTest.groovy
@@ -1,2 +1,27 @@
-class PositiveFilterTest {
+import com.aor.numbers.PositiveFilter
+import spock.lang.Specification
+
+class PositiveFilterTest extends Specification{
+
+    PositiveFilter filter
+
+    def setup() {
+        filter = new PositiveFilter()
+    }
+
+    def "positive input"() {
+        expect:
+        filter.accept(Integer.MAX_VALUE)
+    }
+
+    def "negative input"() {
+        expect:
+        !filter.accept(Integer.MIN_VALUE)
+    }
+
+    def "neutral value"() {
+        expect:
+        !filter.accept(0)
+    }
+
 }
